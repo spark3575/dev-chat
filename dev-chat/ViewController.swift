de592f0eaf3b99f8cde8388a970ad538990e7b83
Shin Park <shinpark@shins-mac-mini.home>
2017-03-31 09:44:10 -0500

    Initial Commit

---
+++ dev-chat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dev-chat
+//
+//  Created by Shin Park on 3/31/17.
+//  Copyright © 2017 shinDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
