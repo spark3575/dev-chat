0d3fd4a832999712b13addf773e68807bc106cf1
Shin Park <shinpark@shins-mac-mini.home>
2017-03-31 10:28:18 -0500

    DevChat - Project creation & using Apple's example code

--- dev-chat/CameraVC.swift
+++ dev-chat/CameraVC.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  CameraVC.swift
 //  dev-chat
 //
 //  Created by Shin Park on 3/31/17.
@@ -8,18 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CameraVC: AAPLCameraViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
